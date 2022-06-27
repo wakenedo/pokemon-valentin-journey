8dbcdfdd0b7efe6285854b4941d3f590976da14a
wakenedo <a_nuner@outlook.com>
2022-06-27 02:34:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { LogoContainer, HomeContainer, SubLogoContainer } from './Styles/global';
 
 
 //Images
 import ValentinJourney from './assets/Home/valentinjourney.png'
 import PokemonLogo from './assets/Home/pokemon-logo-3-1.png'
 
+//Global Styles
+import { LogoContainer, HomeContainer, SubLogoContainer } from './Styles/global';
 
 function App() {
   return (

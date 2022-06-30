70858366b9c4e71efa5f9d3d86672a8b108f8896
wakenedo <a_nuner@outlook.com>
2022-06-30 17:32:23 -0300

    NavBar - Part 4

--- src/Components/NavBar/style.js
+++ src/Components/NavBar/style.js
@@ -13,13 +13,22 @@ display: flex;
 flex-direction: column;
 align-items: center;
 margin: 15px 26px 0 26px;
-padding: 2px;
+padding: 1px;
 cursor: pointer;
+transform: scale(0.9) ;
+transition: all 1s smooth;
+
+
+&:hover {
+transform: scale(1.0);
+transition: all 1s smooth;
+}
 `
 
 export const NavBarImg = styled.img`
-width: 75px;
+width: 60px;
 `
 
 export const NavBarText = styled.p`
+font-size: 14px
 `

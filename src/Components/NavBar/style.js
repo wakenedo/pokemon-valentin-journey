5709d28094209d44c4df43bf2204df058c29407a
wakenedo <a_nuner@outlook.com>
2022-07-05 20:35:47 -0300

    Update style.js

--- src/Components/NavBar/style.js
+++ src/Components/NavBar/style.js
@@ -27,24 +27,6 @@ transition: all 1s smooth;
 }
 `
 
-export const NavBarIcon = styled.a`
-display: flex;
-flex-direction: column;
-align-items: center;
-margin: 15px 26px 0 26px;
-padding: 1px;
-cursor: pointer;
-transform: scale(0.9) ;
-transition: all 1s smooth;
-text-decoration: none;
-
-
-&:hover {
-transform: scale(1.0);
-transition: all 1s smooth;
-}
-`
-
 export const NavBarImg = styled.img`
 width: 60px;
 `

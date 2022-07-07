0d41cfc5abf8c1ad5fcc2f123f0da6d6d973917f
wakenedo <a_nuner@outlook.com>
2022-07-07 00:37:11 -0300

    GlobalLogo, SubLogo & HomeLink to pages - Part 2
    - Games Finished Page
    - Pokemons Captured Page

--- src/pages/GamesFinished/index.js
+++ src/pages/GamesFinished/index.js
@@ -1,6 +1,22 @@
+import ValentinJourney from '../../assets/Home/valentinjourney.png'
+import PokemonLogo from '../../assets/Home/pokemon-logo-3-1.png'
+
+//Global Styles
+import { HomeLink, GlobalLogoContainer, GlobalSubLogoContainer } from '../../Styles/GlobalStyle'
+
 const GamesFinished = () => {
     return (
-        <h1>Games Finished</h1>
+        <>
+            <GlobalLogoContainer>
+                <HomeLink to='/' >
+                    <img src={PokemonLogo} alt='PokemonLogo' />
+                </HomeLink>
+                <GlobalSubLogoContainer>
+                    <img src={ValentinJourney} alt='ValentinJourney' />
+                </GlobalSubLogoContainer>
+            </GlobalLogoContainer>
+            <h1>Games Finished</h1>
+        </>
     )
 }
 export default GamesFinished
--- src/pages/PokemonsCaptured/index.js
+++ src/pages/PokemonsCaptured/index.js
@@ -1,6 +1,22 @@
+import ValentinJourney from '../../assets/Home/valentinjourney.png'
+import PokemonLogo from '../../assets/Home/pokemon-logo-3-1.png'
+
+//Global Styles
+import { HomeLink, GlobalLogoContainer, GlobalSubLogoContainer } from '../../Styles/GlobalStyle'
+
 const PokemonsCaptured = () => {
-    return(
-        <h1>PokemonCaptured</h1>
+    return (
+        <>
+            <GlobalLogoContainer>
+                <HomeLink to='/' >
+                    <img src={PokemonLogo} alt='PokemonLogo' />
+                </HomeLink>
+                <GlobalSubLogoContainer>
+                    <img src={ValentinJourney} alt='ValentinJourney' />
+                </GlobalSubLogoContainer>
+            </GlobalLogoContainer>
+            <h1>PokemonCaptured</h1>
+        </>
     )
 }
 export default PokemonsCaptured
